7c0c1cda4de261653706a5621669ce59c778e291
shinhm1 <shinhm1@naver.com>
2026-06-17 20:49:59 +0900

    feat: 글 페이지 생성 로직 추가

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,6 +10,7 @@
 // You can delete this file if you're not using it
 
 const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
 
 // Setup Import Alias
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
@@ -25,4 +26,77 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
       },
     },
   });
+};
+
+// Generate a Slug Each Post Data
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: 'contents' });
+
+    createNodeField({ node, name: 'slug', value: slug });
+  }
+};
+
+// Ensure optional fields exist in GraphQL schema
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+
+  createTypes(`
+    type MarkdownRemarkFields {
+      slug: String!
+    }
+
+    type MarkdownRemark implements Node {
+      fields: MarkdownRemarkFields
+    }
+  `);
+};
+
+// Generate Post Page Through Markdown Data
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions;
+
+  // Get All Markdown File For Paging
+  const queryAllMarkdownData = await graphql(`
+    {
+      allMarkdownRemark(sort: [{ frontmatter: { date: DESC } }, { frontmatter: { title: ASC } }]) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  // Handling GraphQL Query Error
+  if (queryAllMarkdownData.errors) {
+    reporter.panicOnBuild(`Error while running query`);
+    return;
+  }
+
+  // Import Post Template Component
+  const PostTemplateComponent = path.resolve(__dirname, 'src/templates/post_template.tsx');
+
+  // Page Generating Function
+  const generatePostPage = ({
+    node: {
+      fields: { slug },
+    },
+  }) => {
+    const pageOptions = {
+      path: slug,
+      component: PostTemplateComponent,
+      context: { slug },
+    };
+
+    createPage(pageOptions);
+  };
+
+  // Generate Post Page And Passing Slug Props for Query
+  queryAllMarkdownData.data.allMarkdownRemark.edges.forEach(generatePostPage);
 };
